bbcaa071bdd210b61b587925e9e482469b36aa6d
Nik Fenech <nik.fenech@gmail.com>
2017-10-05 01:46:24 +1100

    DLET

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {	
-	$('#about-button').click( function(){
-	  $('.about').toggleClass("visible");
-	  //$(this).toggleClass("hidden").toggleClass('visible');
-	});
-});	
